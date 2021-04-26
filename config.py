7bdbd6f3ee6ecd816e1623f656c53b7b5ecc2020
bankijey <36775155+bankijey@users.noreply.github.com>
2021-04-26 08:56:46 +0200

    Update config.py
    Remove SQL URI

--- config.py
+++ config.py
@@ -18,18 +18,3 @@ def config(filename= 'config.ini', section='postgresql'):
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
     return db
-
-result = urlparse("postgres://xmedzuylzapgdq:0d4b3758b43de4c7ee01b02646ff7c0348d13b75723656ca88a9f0209cadde07@ec2-107-22-83-3.compute-1.amazonaws.com:5432/d4c25g1h6no7hu")
-# also in python 3+ use: urlparse("YourUrl") not urlparse.urlparse("YourUrl")
-username = result.username
-password = result.password
-database = result.path[1:]
-hostname = result.hostname
-port = result.port
-connection = psycopg2.connect(
-    database = database,
-    user = username,
-    password = password,
-    host = hostname,
-    port = port
-)
